f09c047d09092fc11d4fbb67ed07919f7a399ebe
Daniel <mckeond18@rsu20.org>
2018-02-13 15:27:10 -0500

    Set up auto and driveForward command

---
+++ src/org/usfirst/frc5506/PowerUp/commands/AutoBoring.java
@@ -0,0 +1,11 @@
+package org.usfirst.frc5506.PowerUp.commands;
+
+import edu.wpi.first.wpilibj.command.CommandGroup;
+
+public class AutoBoring extends CommandGroup {
+	public AutoBoring()
+	{
+		//just drive forward bang bang
+	}
+}
+
